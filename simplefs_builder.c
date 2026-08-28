9cc9382da3e5e0f1146e6b15d78513c6ed114e31
Linui <revokeshare@gmail.com>
2026-08-28 15:19:46 +0600

    Update simplefs_builder.c
    Editied the comments

--- simplefs_builder.c
+++ simplefs_builder.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 
     /* TODO 1: Fill all superblock fields. */
     memset(&sb, 0, sizeof(sb));
-    /* TODO: STUDENT CODE START */
+    /* CODE EDITING START'S HERE */
     sb.magic = MAGIC_NUMBER;                    // 0x53465331
     sb.block_size = BLOCK_SIZE;                // 4096
     sb.total_blocks = TOTAL_BLOCKS;            // 64
@@ -40,52 +40,52 @@ int main(int argc, char *argv[])
     sb.inode_table_block = INODE_TABLE_BLOCK;  // 3
     sb.data_region_block = DATA_REGION_BLOCK;  // 4
     sb.root_inode = ROOT_INODE;                // 1
-    /* TODO: STUDENT CODE END */
+    /* TODO END */
     fseek(fp, SUPERBLOCK_BLOCK * BLOCK_SIZE, SEEK_SET);
     fwrite(&sb, sizeof(sb), 1, fp);
 
     /* TODO 2: Mark inode 1 allocated (inode bitmap index 0). */
-    /* TODO: STUDENT CODE START */
+     /* CODE EDITING START'S HERE */
     set_bit(inode_bitmap, ROOT_INODE - 1);     // Index 0 represents Inode 1
-    /* TODO: STUDENT CODE END */
+    /* TODO END */
     fseek(fp, INODE_BITMAP_BLOCK * BLOCK_SIZE, SEEK_SET);
     fwrite(inode_bitmap, BLOCK_SIZE, 1, fp);
 
     /* TODO 3: Mark root data block allocated (data bitmap index 0). */
-    /* TODO: STUDENT CODE START */
+     /* CODE EDITING START'S HERE */
     set_bit(data_bitmap, 0);                  // Index 0 represents Block 4 (DATA_REGION_BLOCK)
-    /* TODO: STUDENT CODE END */
+    /* TODO END */
     fseek(fp, DATA_BITMAP_BLOCK * BLOCK_SIZE, SEEK_SET);
     fwrite(data_bitmap, BLOCK_SIZE, 1, fp);
 
     /* TODO 4: Initialize root inode according to the specification. */
     memset(&root_inode, 0, sizeof(root_inode));
-    /* TODO: STUDENT CODE START */
+     /* CODE EDITING START'S HERE */
     root_inode.type = TYPE_DIRECTORY;          // 2
     root_inode.links = 2;                     // '.' and '..'
     root_inode.size = 2 * DIRENT_SIZE;         // 128 bytes (2 x 64-byte directory entries)
     root_inode.direct[0] = ROOT_DATA_BLOCK;    // Block 4
     root_inode.direct[1] = 0;                  // Unused
     root_inode.direct[2] = 0;                  // Unused
-    /* TODO: STUDENT CODE END */
+    /* TODO END */
     fseek(fp, inode_offset(ROOT_INODE), SEEK_SET);
     fwrite(&root_inode, sizeof(root_inode), 1, fp);
 
     /* TODO 5: Initialize the '.' entry. */
     memset(&dot, 0, sizeof(dot));
-    /* TODO: STUDENT CODE START */
+     /* CODE EDITING START'S HERE */
     dot.inode_no = ROOT_INODE;                 // 1
     dot.type = TYPE_DIRECTORY;                 // 2
     strncpy(dot.name, ".", sizeof(dot.name) - 1);
-    /* TODO: STUDENT CODE END */
+    /* TODO END */
 
     /* TODO 6: Initialize the '..' entry. */
     memset(&dotdot, 0, sizeof(dotdot));
-    /* TODO: STUDENT CODE START */
+     /* CODE EDITING START'S HERE */
     dotdot.inode_no = ROOT_INODE;              // 1 (root directory's parent is itself)
     dotdot.type = TYPE_DIRECTORY;              // 2
     strncpy(dotdot.name, "..", sizeof(dotdot.name) - 1);
-    /* TODO: STUDENT CODE END */
+    /* TODO END */
 
     fseek(fp, ROOT_DATA_BLOCK * BLOCK_SIZE, SEEK_SET);
     fwrite(&dot, sizeof(dot), 1, fp);
